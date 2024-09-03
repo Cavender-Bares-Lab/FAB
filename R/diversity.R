01911289f41fe64045d51178cf1d5f1435148e2c
Guzmán Q. <aguzman@fas.harvard.edu>
2024-09-03 10:08:08 -0400

    Adding Jeannine code

--- R/diversity.R
+++ R/diversity.R
@@ -8,16 +8,24 @@
 #' The last column should be the metric used to weight diversity estimations
 #' (e.g., DBH, above ground biomass, or volume).
 #'
-#' @param tree A \code{phylo} tree object,  or a phylogenetic covariance matrix.
+#' @param tree A \code{phylo} tree object, or a phylogenetic covariance matrix.
+#'
+#' @param functional_traits A \code{data.table} or \code{data.frame} with functional
+#' trait data per columns and species rows. The first column must be labeled as
+#' 'species', with species that match those from \code{inventory}.
+#'
+#' @param scale_traits Logic. If \code{TRUE} and \code{functional_traits} is not \code{NULL},
+#' it scales \code{traits}.
 #'
 #' @details
 #' The function first summarize forest inventories, then it calculate metrics
-#' of species richness (SR), phylogenetic species variability (PSV),
-#' phylogenetic species richness (PSR), phylogenetic species evenness (PSE),
-#' phylogenetic species clustering (PSC), and Faith's phylogenetic diversity. Similar
-#' metrics can be derived on functional information, but using an \code{hclust} tree
-#' of similarity of traits among species. If you use this function in your work,
-#' please also acknowledge \code{picante} package.
+#' of species richness (SR), phylogenetic or functional species variability (PSV or FSV),
+#' phylogenetic or functional species richness (PSR or FSR), phylogenetic or Functional
+#' species evenness (PSE or FSE), phylogenetic or functional species clustering (PSC or FSC),
+#' and Faith's phylogenetic or functional diversity. Similar metrics can be
+#' derived on functional information, but using an \code{hclust} tree
+#' of similarity of traits among species folloiwing (Cavender-Bares et al. 2012).
+#' If you use this function in your work, please also acknowledge \code{picante} package.
 #'
 #' @references
 #' Faith D.P. (1992) Conservation evaluation and phylogenetic diversity.
@@ -26,12 +34,17 @@
 #' Helmus M.R., Bland T.J., Williams C.K. & Ives A.R. (2007) Phylogenetic
 #' measures of biodiversity. American Naturalist, 169, E68-E83
 #'
-#' Cavender-Bares, J., and P. B. Reich. 2012. Shocks to the system: Community assembly of the oak savanna in a 40‐year fire frequency experiment. Ecology 93:S52 - S69.
-#' Cadotte, M. W., J. Cavender-Bares, D. Tilman, and T. H. Oakley. 2009. Using phylogenetic, functional and trait diversity to understand patterns of plant community productivity. Plos One 4:e5695.
+#' Cavender-Bares, J., and P. B. Reich. 2012. Shocks to the system: Community
+#' assembly of the oak savanna in a 40‐year fire frequency experiment.
+#' Ecology 93:S52 - S69.
+#'
+#' Cadotte, M. W., J. Cavender-Bares, D. Tilman, and T. H. Oakley. 2009. Using
+#' phylogenetic, functional and trait diversity to understand patterns of plant
+#' community productivity. Plos One 4:e5695.
 #'
 #' @returns It returns a \code{data.table} with metrics of diversity.
 #'
-#' @author J. Antonio Guzmán Q.
+#' @author Jeannine Cavender-Bares and J. Antonio Guzmán Q.
 #'
 #' @importFrom data.table data.table
 #' @importFrom picante psv
@@ -42,8 +55,15 @@
 #'
 #' @examples
 diversity <- function(inventory,
-                      tree = NULL,
-                      weight = c("abundance", "n_indiviuals")) {
+                      phylo_tree = NULL,
+                      functional_matrix = NULL,
+                      weight = c("abundance", "n_indiviuals"),
+                      scale_traits = FALSE) {
+
+  # Test presence of datasets
+  if(is.null(phylo_tree) == TRUE & is.null(functional_matrix) == TRUE) {
+    stop("Please provide a phylo_tree or a functional matrix")
+  }
 
   # Make a copy and rename
   frame <- inventory
